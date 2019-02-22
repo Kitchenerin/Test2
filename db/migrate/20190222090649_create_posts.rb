2bfbcd6c4f5be6961674bbd7ecbf798c3d5f1115
Kitchenerin <lenyajoker66@gmail.com>
2019-02-22 12:58:02 +0300

    ORM output on browser

--- db/migrate/20190222090649_create_posts.rb
+++ db/migrate/20190222090649_create_posts.rb
@@ -1,8 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.text :body
+      t.string :name
+      t.string :family_name
+      t.string :email
+      t.text :address
+      t.text :description
 
       t.timestamps
     end
